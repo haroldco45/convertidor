9f6c1fa4f2bd410d2c06123842c1eb235c8d6270
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-03-09 09:14:56 -0500

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -2,20 +2,35 @@
 if (typeof window === "undefined") {
     self.addEventListener("install", () => self.skipWaiting());
     self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
     self.addEventListener("fetch", (event) => {
-        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+            return;
+        }
+
         event.respondWith(
-            fetch(event.request).then((response) => {
-                if (response.status === 0) return response;
-                const newHeaders = new Headers(response.headers);
-                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
-                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-                return new ImageResponse(response.body, { status: response.status, statusText: response.statusText, headers: newHeaders });
-            }).catch(() => fetch(event.request))
+            fetch(event.request)
+                .then((response) => {
+                    if (response.status === 0) {
+                        return response;
+                    }
+
+                    const newHeaders = new Headers(response.headers);
+                    newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                    newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                    return new Response(response.body, {
+                        status: response.status,
+                        statusText: response.statusText,
+                        headers: newHeaders,
+                    });
+                })
+                .catch((e) => fetch(event.request))
         );
     });
 } else {
-    const script = document.createElement("script");
-    script.src = "https://cdnjs.cloudflare.com/ajax/libs/coi-serviceworker/0.1.7/coi-serviceworker.min.js";
-    document.head.appendChild(script);
+    // Si se carga en el hilo principal, registra el service worker
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register(window.document.currentScript.src);
+    }
 }
